ab2544374bddfe7fd0b2eecccd4a208a184c9287
Lafwind <lafwind@gmail.com>
2015-08-20 23:58:48 +0800

    update

--- custom.el
+++ custom.el
@@ -6,14 +6,14 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (smart-tabs-mode yasnippet writeroom-mode web-mode twilight-theme twilight-anti-bright-theme swiper sublime-themes subatomic-theme sql-indent spacemacs-theme spacegray-theme solarized-theme smex scss-mode rust-mode robe rinari projectile-rails powershell powerline-evil popup paredit neotree multiple-cursors material-one-theme markdown-mode magit js2-mode indent-guide ido-vertical-mode highlight-parentheses highlight-indentation helm-projectile helm-git haskell-mode haml-mode golden-ratio geiser flycheck flx-ido flatland-theme flatland-black-theme find-file-in-project fill-column-indicator expand-region evil-nerd-commenter evil-leader emmet-mode eclipse-theme cyberpunk-theme company-inf-ruby color-theme-sanityinc-tomorrow col-highlight coffee-mode base16-theme atom-one-dark-theme ample-theme airline-themes aggressive-indent))))
+    (isearch-switch relative-line-numbers linum-relative hlinum helm-ls-git evil-surround evil-easymotion easy-kill smart-tabs-mode yasnippet writeroom-mode web-mode twilight-theme twilight-anti-bright-theme swiper sublime-themes subatomic-theme sql-indent spacemacs-theme spacegray-theme solarized-theme smex scss-mode rust-mode robe rinari projectile-rails powershell powerline-evil popup paredit neotree multiple-cursors material-one-theme markdown-mode magit js2-mode indent-guide ido-vertical-mode highlight-parentheses highlight-indentation helm-projectile helm-git haskell-mode haml-mode golden-ratio geiser flycheck flx-ido flatland-theme flatland-black-theme find-file-in-project fill-column-indicator expand-region evil-nerd-commenter evil-leader emmet-mode eclipse-theme cyberpunk-theme company-inf-ruby color-theme-sanityinc-tomorrow col-highlight coffee-mode base16-theme atom-one-dark-theme ample-theme airline-themes aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-scrollbar-bg ((t (:background "#333333"))))
- '(company-scrollbar-fg ((t (:background "#262626"))))
- '(company-tooltip ((t (:inherit default :background "#1f1f1f"))))
+ '(company-scrollbar-bg ((t (:background "#3a3a3a"))))
+ '(company-scrollbar-fg ((t (:background "#2d2d2d"))))
+ '(company-tooltip ((t (:inherit default :background "#262626"))))
  '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
  '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
